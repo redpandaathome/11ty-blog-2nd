544a725803b6ee1c5633d93dda0ceefae347300f
redpandaathome <leeyumi415@gmail.com>
2022-07-18 17:31:39 -0500

    changed passthroguh path

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-   eleventyConfig.addPassthroughCopy('src/style.css')
-   eleventyConfig.addPassthroughCopy('src/assets')
+   eleventyConfig.addPassthroughCopy('./src/style.css')
+   eleventyConfig.addPassthroughCopy('./src/assets')
    eleventyConfig.addPassthroughCopy("./src/admin");
    
    eleventyConfig.addFilter("postDate", (dateObj) => {
